037dd76745fd5a476f70b230a752b55c2f24e32a
Agnė Klimaitė <agnaite@me.com>
2017-05-31 14:59:02 -0700

    incrementing works

--- app/assets/javascripts/components/_all_days.js.jsx
+++ app/assets/javascripts/components/_all_days.js.jsx
@@ -1,11 +1,13 @@
 var AllDays = React.createClass({
+  onUpdate(day) {
+    this.props.onUpdate(day);
+  },
   render() {
     var days = this.props.days.map((day) => {
       return (
         <div key={day.id}>
           <Day day={day}
-               addDay={this.props.addDay}
-          />
+               handleUpdate={this.onUpdate} />
         </div>
       )
     });
--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -5,15 +5,26 @@ var Body = React.createClass({
   componentDidMount(){
     $.getJSON('/api/v1/days.json', (response) => { this.setState({ days: response }) });
   },
-  addDay(){
-    console.log('hi');
+  handleUpdate(day){
+    $.ajax({
+           url: `/api/v1/days/${day.id}`,
+           type: 'PUT',
+           data: { day: day },
+           success: () => {
+              this.updateDays(day)
+           }
+       })
+  },
+  updateDays(day) {
+    var days = this.state.days.filter((i) => { return i.id != day.id });
+    days.push(day);
+    this.setState({ days: days });
   },
   render(){
     return (
       <div>
         <AllDays days={this.state.days}
-                 addDay={this.addDay}
-        />
+                 onUpdate={this.handleUpdate} />
       </div>
     )
   }
